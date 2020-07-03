ebcc608a51fd9c98e9d46563c1e37602c37e12bb
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-07-03 14:52:57 +1200

    implemented account creation

--- banking.py
+++ banking.py
@@ -7,12 +7,32 @@ def mainmenu():
     print("0. Exit")
 
 
+iin = "400000"
+account = ""
+pin = ""
+cards = {}
+
 while True:
     mainmenu()
     action = input()
 
     if action == "1":
-        pass
+        for i in range(9):
+            account += str(random.randint(0, 9))
+        checksum = str(random.randint(0, 9))
+        card_number = iin + account + checksum
+
+        for j in range(4):
+            pin += str(random.randint(0, 9))
+
+        cards.update({card_number: pin})
+
+        print("Your card has been created")
+        print("Your card number:")
+        print(card_number)
+        print("Your card PIN")
+        print(pin)
+        print()
 
     elif action == "2":
         pass
