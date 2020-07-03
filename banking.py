48d3eead6a0124cfa094d0a8d2718b7e6959d117
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-07-03 14:37:17 +1200

    implemented main menu and exit

--- banking.py
+++ banking.py
@@ -1,1 +1,21 @@
 # Write your code here
+import random
+
+def mainmenu():
+    print("1. Create and account")
+    print("2. Log into account")
+    print("0. Exit")
+
+
+while True:
+    mainmenu()
+    action = input()
+
+    if action == "1":
+        pass
+
+    elif action == "2":
+        pass
+
+    elif action == "0":
+        break
